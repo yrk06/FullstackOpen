6e2e2b82540ec81ae8093208d1d2f75c293e6bbf
Yerik <yerik.koslowski@gmail.com>
2022-02-17 23:15:39 -0300

    Finished Courseinfor activity

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,42 @@
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const sum = course.parts.reduce((prev,current)=>{
+      return prev+current.exercises
+    },0)
+    return(
+      <p>Number of exercises {sum}</p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    return (
+      <div>
+        {course.parts.map((part)=> <Part key={part.id} part={part} /> ) }
+      </div>
+    )
+  }
+  
+  const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+export default Course
